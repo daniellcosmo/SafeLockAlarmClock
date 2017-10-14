0cd699c7c02070921b3144e4a632a3f75766374b
pudim <pudim@pudim-PC>
2017-10-14 17:16:48 -0300

    Alterações na SegundaActivity Centralização da Fechadura Segunda alternativa para destracar aparece após 1 minuto

--- CofreAlarme/gen/com/example/cofrealarme/R.java
+++ CofreAlarme/gen/com/example/cofrealarme/R.java
@@ -10,6 +10,9 @@ package com.example.cofrealarme;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int cinza=0x7f050000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,8 +21,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f070000;
+        public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
         public static final int frente=0x7f020000;
@@ -28,35 +31,38 @@ public final class R {
         public static final int medio=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
-        public static final int end_alarm=0x7f090005;
-        public static final int imageView1=0x7f090000;
-        public static final int imageView2=0x7f090001;
-        public static final int start_alarm=0x7f090004;
-        public static final int timePicker=0x7f090002;
-        public static final int update_text=0x7f090003;
+        public static final int action_settings=0x7f0a0008;
+        public static final int button1=0x7f0a0002;
+        public static final int editText1=0x7f0a0003;
+        public static final int end_alarm=0x7f0a0007;
+        public static final int imageView1=0x7f0a0000;
+        public static final int imageView2=0x7f0a0001;
+        public static final int start_alarm=0x7f0a0006;
+        public static final int timePicker=0x7f0a0004;
+        public static final int update_text=0x7f0a0005;
     }
     public static final class layout {
         public static final int activity_cofre=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int cofre=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int cofre=0x7f090000;
+        public static final int main=0x7f090001;
     }
     public static final class raw {
         public static final int alarme1=0x7f040000;
         public static final int alarme2=0x7f040001;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int end_alarm=0x7f060004;
-        public static final int hello_world=0x7f060001;
-        public static final int start_alarm=0x7f060003;
-        public static final int title_activity_cofre=0x7f060007;
-        public static final int update=0x7f060006;
-        public static final int update_text=0x7f060005;
+        public static final int action_settings=0x7f080002;
+        public static final int app_name=0x7f080000;
+        public static final int desativar=0x7f080008;
+        public static final int end_alarm=0x7f080004;
+        public static final int hello_world=0x7f080001;
+        public static final int start_alarm=0x7f080003;
+        public static final int title_activity_cofre=0x7f080007;
+        public static final int update=0x7f080006;
+        public static final int update_text=0x7f080005;
     }
     public static final class style {
         /** 
@@ -80,10 +86,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060001;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060002;
+        public static final int textView1_css=0x7f060000;
     }
 }
--- CofreAlarme/src/com/example/cofrealarme/CofreActivity.java
+++ CofreAlarme/src/com/example/cofrealarme/CofreActivity.java
@@ -1,16 +1,49 @@
 package com.example.cofrealarme;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class CofreActivity extends Activity {
 
+	Button button1;
+	EditText editText1;
+	
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cofre);
+		
+		button1 = (Button) findViewById (R.id.button1);
+		editText1 = (EditText) findViewById (R.id.editText1);
+		
+		button1.setVisibility(View.INVISIBLE);
+		editText1.setVisibility(View.INVISIBLE);
+		
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                button1.setVisibility(View.VISIBLE);
+                editText1.setVisibility(View.VISIBLE);
+            }
+        }, 60000); 	// 60 segundos
+        
+        
+	
+		
+		
+		
 	}
 
 	@Override
